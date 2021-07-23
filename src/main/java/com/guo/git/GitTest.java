c5ba78b904880ad365980e2459425cddee217361
zqhwboy <80964237+zqhwboy@users.noreply.github.com>
2021-07-23 21:57:56 +0800

    pull commit

--- src/main/java/com/guo/git/GitTest.java
+++ src/main/java/com/guo/git/GitTest.java
@@ -9,5 +9,7 @@ public class GitTest {
         System.out.println("master test!");
         System.out.println("master test!");
         System.out.println("push  test!");
+        System.out.println("pull  test!");
+        
     }
 }
